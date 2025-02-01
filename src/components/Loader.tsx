65152ab0827afdffd11d96ba1c2f8f7ec7e29cf9
xlaoru <sia212007@gmail.com>
2025-02-01 23:15:57 +0200

    new-loader-design-creation

---
+++ src/components/Loader.tsx
@@ -0,0 +1,3 @@
+export default function Loader() {
+    return <div className="container-absolute-center" style={{ height: "85vh" }}><div className="loader"></div></div>;
+}
--- src/pages/ProgramListPage.tsx
+++ src/pages/ProgramListPage.tsx
@@ -4,24 +4,30 @@ import ProgramTable from "../components/ProgramTable";
 import { getPrograms } from "../services";
 
 import { Program } from "../utils/models";
+import Loader from "../components/Loader";
 
 export default function ProgramListPage() {
     const [programs, setPrograms] = useState<Program[] | []>([]);
+    const [hasLoaded, setLoaded] = useState(false)
 
     useEffect(() => {
         getPrograms().then((response) => {
             setPrograms(response.data);
+            setLoaded(true)
         });
     }, []);
 
     return (
         <>
-            {programs && programs.map((program, index) => (
-                <div className="container" key={index}>
-                    <ProgramTable program={program} />
-                    <hr className="separator" />
-                </div>
-            ))}
+            {(programs && hasLoaded)
+                ? programs.map((program, index) => (
+                    <div className="container" key={index}>
+                        <ProgramTable program={program} />
+                        <hr className="separator" />
+                    </div>
+                ))
+                : <Loader />
+            }
         </>
     )
 }
