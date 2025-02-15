9a43fdcd0a33e994423c3547849ab55f477e73a0
xlaoru <sia212007@gmail.com>
2025-02-15 21:09:17 +0200

    removing-extra-code

--- src/services/index.ts
+++ src/services/index.ts
@@ -7,27 +7,4 @@ const api = axios.create({
     baseURL: API_URL
 })
 
-// ! DEMO
-/* api.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
-    return config;
-})
-
-api.interceptors.response.use((config) => {
-    return config;
-}, async (error) => {
-    const originalRequest = error.config;
-    if (error.response.status === 403 && error.config && !error.config._isRetry) {
-        originalRequest._isRetry = true;
-        try {
-            const response = await axios.post(`${API_URL}/auth/refresh`, {}, {withCredentials: true})
-            localStorage.setItem('token', response.data.token);
-            return api.request(originalRequest);
-        } catch (e) {
-            console.log('Not authorized.')
-        }
-    }
-    throw error;
-}) */
-
 export default api
